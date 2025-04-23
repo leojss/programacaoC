5b7a7ac345c312ce3b66fc8f67cac385dacd9405
Leonardo Silva <leo.jss@outlook.com>
2025-04-23 09:15:18 -0300

    alterar tipo de variavel de populacao, para unsign long int

--- olamundo.c
+++ olamundo.c
@@ -3,7 +3,8 @@
 int main() {
     // Declaração de todas as variáveis
     char estado1, estado2, codigo1[20], nomeCidade1[100], codigo2[20], nomeCidade2[100];
-    int populacao1, numPontosTuristicos1, populacao2, numPontosTuristicos2;
+    unsigned long int populacao1, populacao2;
+    int numPontosTuristicos1, numPontosTuristicos2;
     float area1, pib1, area2, pib2;
     float densidade1, pibPerCapita1, densidade2, pibPerCapita2;
 
@@ -25,7 +26,7 @@ int main() {
     fgets(nomeCidade1, sizeof(nomeCidade1), stdin);
 
     printf("Digite a população: ");
-    scanf(" %d", &populacao1);
+    scanf(" %lu", &populacao1);
 
     printf("Digite a área: ");
     scanf(" %f", &area1);
@@ -58,7 +59,7 @@ int main() {
     fgets(nomeCidade2, sizeof(nomeCidade2), stdin);
 
     printf("Digite a população: ");
-    scanf(" %d", &populacao2);
+    scanf(" %lu", &populacao2);
 
     printf("Digite a área: ");
     scanf(" %f", &area2);
@@ -80,7 +81,7 @@ int main() {
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s", nomeCidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", numPontosTuristicos1);
@@ -91,7 +92,7 @@ int main() {
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s", nomeCidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", numPontosTuristicos2);
